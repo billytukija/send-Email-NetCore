626c2ae294bd67751cead58c7598aa432692038e
Billy Tukija Richard <billy.tukija@onlineapp.com.br>
2019-11-27 01:05:33 -0200

    Simple project using smtp for send email

---
+++ send-Email-NetCore2/Program.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+
+namespace send_Email_NetCore2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Billy", "billytukija@gmail.com"));
+            message.To.Add(new MailboxAddress("Grazzy", "graziellytukija@beapps.com.br"));
+            message.Subject = "Aprovação";
+            message.Body = new TextPart("plain")
+            {
+                Text = "Hey Grazzy .. "
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("billytukija@gmail.com", "#########");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
